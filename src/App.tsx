f20eb2357a51b13486a9977d5631f7155d854f8a
yadavn01 <ynaman29@gmail.com>
2023-04-18 21:11:47 +1200

    routes setup and nav bar working

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { Routes, Route } from "react-router-dom"
 import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
+import { About } from "./pages/About"
+import { Store } from "./pages/Store"
+import { Navbar } from "./components/Navbar"
 
 function App() {
   return (
-   <Container> 
+    <>
+    <Navbar />
+   <Container className="mb-4"> 
     <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/store" element={<Store />} />
           <Route path="/about" element={<About />} />
         </Routes>
    </Container>
+   </>
   )
 }
 
