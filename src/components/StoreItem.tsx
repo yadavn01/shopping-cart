b33adfba72c8e348c25f95cfbe71f1fa4aa1ed8c
yadavn01 <ynaman29@gmail.com>
2023-04-18 22:48:29 +1200

    add to cart button template setup

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import {Card} from "react-bootstrap"
+import { Button } from "react-bootstrap"
 import { formatCurrency } from "../utilities/formatCurrency"
 
 type StoreItemProps = {
@@ -9,8 +10,9 @@ type StoreItemProps = {
 }
 
 export function StoreItem({id, name, price, imgUrl}:StoreItemProps) {
+    const quantity = 0;
     return (
-        <Card>
+        <Card className="h-100">
             <Card.Img variant="top" src={imgUrl} height="200px"
         style={{ objectFit: "cover" }} />
 
@@ -19,7 +21,27 @@ export function StoreItem({id, name, price, imgUrl}:StoreItemProps) {
           <span className="fs-2">{name}</span>
           <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
-
+        <div className="mt-auto">
+            {quantity === 0 ? (
+                <Button className="w-100"> + Add to Cart</Button>
+            ) : (
+                <div
+              className="d-flex align-items-center flex-column"
+              style={{ gap: ".5rem" }}
+            >
+                <div
+                className="d-flex align-items-center justify-content-center"
+                style={{ gap: ".5rem" }}
+              >
+                <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
+                <div>
+                  <span className="fs-3">{quantity}</span> in cart
+                </div>
+                <Button onClick={() => increaseCartQuantity(id)}>+</Button>
+              </div>
+                </div>
+            )}
+        </div>
         </Card.Body>
         </Card>
     )
